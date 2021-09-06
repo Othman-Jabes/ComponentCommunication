9d1787b6d574ce57eccb5ec603ed0879b2d25622
othman-jabes <otman.jabes@gmail.com>
2021-09-06 21:06:14 +0300

    new  edid aaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  name = 'Othman Jabes';
+  val = 'My Nice Input';
   title = 'ComponentCommunication';
+  num=5;
+  red = true;
+  green = false;
+  bgcolor = false;
+  textValue = '';
+  inputValue(){
+    return 'Value From Function';
+  }
+  changeColor(){
+    this.green = !this.green;
+    this.red = !this.red;
+  }
+  changeBackgroundColor(){
+    this.bgcolor = !this.bgcolor;
+  }
+  onClickButton(el: any){
+    console.log(el)
+  }
+  showMsg(event : any){
+    this.textValue = event;
+    if (this.textValue === 'Othman'){
+      alert('Welcome ' + this.textValue);
+    }
+  }
 }
